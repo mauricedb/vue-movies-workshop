656bb53380ba583aff3ca668f91208552e17e1f2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-26 10:51:47 +0200

    Vue router 3 to 4

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import MovieList from "../views/MovieList.vue";
 import MovieDetails from "../views/MovieDetails.vue";
-
-Vue.use(VueRouter);
 
 const routes = [
   {
@@ -49,9 +46,8 @@ const routes = [
   },
 ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
