a2ddf2fb0fb165e7b5e2524296afb5c9412e0fcd
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-26 09:46:09 +0200

    Added MovieCard

---
+++ src/views/MovieCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'MovieDetails', params: { id: movie.id, type } }">
+      {{ movie.title }}
+      <span :title="movie.vote_average">
+        ({{ movie.vote_average | stars }})
+      </span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: Object,
+    type: String,
+  },
+  filters: {
+    stars(value) {
+      return "".padEnd(value / 2, "★").padEnd(5, "☆");
+    },
+  },
+};
+</script>
+
+<style scoped>
+a:link,
+a:visited {
+  color: blue;
+}
+</style>
