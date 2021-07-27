a6e429c4003fd5dac5756d058fb1e6c57c4787cc
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-27 13:36:59 +0200

    Adding the onUnmounted lifecycle hook

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -3,15 +3,19 @@
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, onUnmounted } from "vue";
 
 export default {
   setup() {
     const now = ref(new Date());
 
-    setInterval(() => {
+    const handle = setInterval(() => {
       now.value = new Date();
     }, 1000);
+
+    onUnmounted(() => {
+      clearInterval(handle);
+    });
 
     const time = computed(() => now.value.toLocaleTimeString());
 
