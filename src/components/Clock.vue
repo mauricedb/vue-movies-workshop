4296a83c295b3aa31dbcad2dc8c302ee5733761d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-07-27 12:44:56 +0200

    Reactive clock

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,11 +1,17 @@
 <template>
-  <span>{{ now }}</span>
+  <span>{{ now.toLocaleTimeString() }}</span>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   setup() {
-    const now = new Date().toLocaleTimeString();
+    const now = ref(new Date());
+
+    setInterval(() => {
+      now.value = new Date();
+    }, 1000);
 
     return {
       now,
