626c1fcaf34f889004bffe96e4b3ab85dc56f5cd
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-09-01 10:20:31 +0200

    vue add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@typescript-eslint/parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -18,6 +22,7 @@ module.exports = {
       },
     ],
   },
+
   overrides: [
     {
       files: [
@@ -28,5 +33,12 @@ module.exports = {
         mocha: true,
       },
     },
+  ],
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "@vue/typescript",
   ],
 };
